26814c25e302dfafdb908bfcf2bdb9aaa7165cb3
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-17 11:57:08 +0530

    Add files via upload

---
+++ sunil.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ void main()
+ { int a, b,c;
+  a=10;
+  b=4;
+  c=3;
+  a+=b; // 14 4 3
+  b-=c; // 14 1 3
+  c*=a; // 14 1 42
+  a/=b; // 14 1 42
+  b*=c; // 14 42 42
+  a+=b;
+  
+  
+  
+ 
+  printf("%d %d %d",a,b,c);
+ }
